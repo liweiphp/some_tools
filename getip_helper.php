a2490b6fb90bf26b40ca08b8c38b3a8f7115a1d1
we3613040 <liweiphp@163.com>
2016-02-24 16:48:34 +0800

    update

--- getip_helper.php
+++ getip_helper.php
@@ -7,14 +7,14 @@
 */
 
 function get_ip(){
-	if(getenv("HTTP_CLIENT_IP") && strcasecmp(getenv("HTTP_CLIENT_IP"),"unknown")){
-		$ip = getenv();
-	}else if(getenv("HTTP_X_FORWARDED_FOR") && strcasecmp(getenv("HTTP_X_FORWARDED_FOR"),"unknown" )){
-		$ip = getenv("HTTP_X_FORWARDED_FOR");
-	}else if(getenv("REMOTE_ADDR") && strcasecmp(getenv("REMOTE_ADDR"),"unknown")){
+	if(getenv("REMOTE_ADDR") && strcasecmp(getenv("REMOTE_ADDR"),"unknown")){
 		$ip = getenv("REMOTE_ADDR");
 	}else if(isset($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR'] && strcasecmp($_SERVER['REMOTE_ADDR'], "unknown")){
 		$ip = $_SERVER['REMOTE_ADDR'];
+	}else if(getenv("HTTP_CLIENT_IP") && strcasecmp(getenv("HTTP_CLIENT_IP"),"unknown")){
+		$ip = getenv();
+	}else if(getenv("HTTP_X_FORWARDED_FOR") && strcasecmp(getenv("HTTP_X_FORWARDED_FOR"),"unknown" )){
+		$ip = getenv("HTTP_X_FORWARDED_FOR");
 	}else{
 		$ip = 'unknown';
 	}
