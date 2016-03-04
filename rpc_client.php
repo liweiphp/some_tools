a92beab10ba0b4210beb7d9fc52672049709ea22
we3613040 <liweiphp@163.com>
2016-03-04 21:41:29 +0800

    ci实现的rpc

--- rpc_client.php
+++ rpc_client.php
@@ -2,15 +2,15 @@
 /**
 * 
 */
-class rpc_client extends PT_Controller
+class rpc_client extends CI_Controller
 {
 	public function index(){
-		$server_url = "http://i.ptbus.com/rpcdemo/server";
+		$server_url = "http://localhost/ci/rpcdemo/server";
         $this->load->library('xmlrpc');
-        $this->xmlrpc->debug = TRUE;
+        //$this->xmlrpc->debug = TRUE;
         $this->xmlrpc->server($server_url, 80);
-        $this->xmlrpc->method('test');
-        $request = array('How is it going?');
+        $this->xmlrpc->method('hello');
+        $request = array('How is it going?',"wocao");
         $this->xmlrpc->request($request);
         if ( ! $this->xmlrpc->send_request())
         {
--- rpcdemo.php
+++ rpcdemo.php
@@ -2,23 +2,23 @@
 /**
 * rpc
 */
-class rpcdemo extends PT_Controller
+class rpcdemo extends CI_Controller
 {
 	public function server(){
 		$this->load->library("xmlrpc");
 		$this->load->library("xmlrpcs");
-		$config["function"]["test"] = array("function"=>"rpcdemo.test");
-		$config['object'] = $this;
+		$config["functions"]["hello"] = array("function"=>"rpcdemo.test");
+		//$config['object'] = $this;
 		$this->xmlrpcs->initialize($config);
-		$url = "http://i.ptbus.com/rpcdemo/server";
 		$this->xmlrpcs->serve();
 	}
 	public function test($request){
 		$parameters = $request->output_parameters();
         $response = array(
             array(
-                'you_said'  => $parameters[0],
-                'i_respond' => 'Not bad at all.'
+                'you_said'  => $parameters[0]
+                ,'i_respond' => 'Not bad at all.'
+                ,'just'=>$parameters[1]
             ),
             'struct'
         );
