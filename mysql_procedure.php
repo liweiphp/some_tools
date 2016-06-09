8ce6f200d45fbdb8dd9c28ebbae3d9350a5377ce
we3613040 <liweiphp@163.com>
2016-06-09 15:13:04 +0800

    存储过程

--- mysql_procedure.php
+++ mysql_procedure.php
@@ -22,3 +22,34 @@ create event if not exists event_log on schedule
 every 100 second
 on completion preserve
 do call utable ('log','isread');
+
+CREATE PROCEDURE `test`.`new_procedure` ()
+BEGIN
+-- 需要定义接收游标数据的变量 
+  DECLARE a CHAR(16);
+  -- 游标
+  DECLARE cur CURSOR FOR SELECT i FROM test.t;
+  -- 遍历数据结束标志
+  DECLARE done INT DEFAULT FALSE;
+  -- 将结束标志绑定到游标
+  DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+  -- 打开游标
+  OPEN cur;
+  
+  -- 开始循环
+  read_loop: LOOP
+    -- 提取游标里的数据，这里只有一个，多个的话也一样；
+    FETCH cur INTO a;
+    -- 声明结束的时候
+    IF done THEN
+      LEAVE read_loop;
+    END IF;
+    -- 这里做你想做的循环的事件
+
+    INSERT INTO test.t VALUES (a);
+
+  END LOOP;
+  -- 关闭游标
+  CLOSE cur;
+
+END
